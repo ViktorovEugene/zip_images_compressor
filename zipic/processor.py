dffa006ec74bb447e35263eb0bb4b4bf437bd7c1
Eugene Viktorov <eugene.viktorov@anvileight.com>
2016-05-23 18:32:59 +0300

    Implelent ZIP's CRC inspection
    Check ZIP file's content on correctness of the control sums.

--- zipic/processor.py
+++ zipic/processor.py
@@ -2,10 +2,20 @@ import zipfile
 import logging
 
 
-def test_zip(zip_name):
-    logging.info('"%s" is zip file => %s', zip_name,
-                 zipfile.is_zipfile(zip_name))
+def check_if_its_zip(zip_name):
+    if not zipfile.is_zipfile(zip_name):
+        raise TypeError('Expected namely ZIP file.')
+    logging.info('"%s" is zip file', zip_name)
+
+
+def check_zips_for_errors(zip_obj):
+    bad_zip_content = zip_obj.testzip()
+    if bad_zip_content:
+        raise zipfile.BadZipFile("The %s of this ZIP file has the bad CRC." %
+                                 bad_zip_content)
 
 
 def process_zip(zip_name):
-    test_zip(zip_name)
+    check_if_its_zip(zip_name)
+    with zipfile.ZipFile(zip_name, 'r') as zip_obj:
+        check_zips_for_errors(zip_obj)
