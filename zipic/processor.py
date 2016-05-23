0f48fa5872b44a8e51f0b6d214de703c72339672
Eugene Viktorov <eugene.viktorov@anvileight.com>
2016-05-23 18:41:05 +0300

    Retrive PNG-file names from given zip-file

--- zipic/processor.py
+++ zipic/processor.py
@@ -15,7 +15,13 @@ def check_zips_for_errors(zip_obj):
                                  bad_zip_content)
 
 
+def get_zips_png_file_names(zip_object):
+    return [name for name in zip_object.namelist() if name[-4:] == '.png']
+
+
 def process_zip(zip_name):
     check_if_its_zip(zip_name)
     with zipfile.ZipFile(zip_name, 'r') as zip_obj:
         check_zips_for_errors(zip_obj)
+        zips_png_names = get_zips_png_file_names(zip_obj)
+    logging.info("PNG files in given zip =>\n%s", ', '.join(zips_png_names))
